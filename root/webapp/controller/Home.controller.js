b67eab375b54eebf4a2d375f79e8b9dc6beb403b
Marc <marcuhart@gmail.com>
2016-11-21 11:47:17 +0100

    add create update et remove odata method

--- root/webapp/controller/Home.controller.js
+++ root/webapp/controller/Home.controller.js
@@ -3,6 +3,7 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel"
 ], function(BaseController, JSONModel) {
     "use strict";
+    var aMethodType = ["Read", "Create", "Update", "Remove"];
 
     return BaseController.extend("sapui5.tools.odatavalidator.controller.Home", {
 
@@ -25,7 +26,9 @@ sap.ui.define([
                 version: this.getResourceBundle().getText("home.sapui5Version", sap.ui.version),
                 language: sap.ui.getCore().getConfiguration().getLanguage(),
                 response: "",
-                request: ""
+                query: "",
+                requestType: 0,
+                jsonText: ""
             });
             this.setModel(oViewModel, "homeViewModel");
         },
@@ -78,12 +81,23 @@ sap.ui.define([
         /* =========================================================== */
         /* Gestionnaire d'événements                                   */
         /* =========================================================== */
+        
+        /**
+         * TODO : comment
+         * @function
+         * @param {sap.ui.base.Event} oEvent
+         * @public
+         */
+        onSelectedItem: function(oEvent) {
+            var iIndex = oEvent.getParameters().selectedIndex;
+            this.getModel("homeViewModel").setProperty("/requestType", iIndex);
+        },
 
         /**
          * TODO : comment
          * @function
          * @param {sap.ui.base.Event} oEvent
-         * @private
+         * @public
          */
         onAboutPressed: function(oEvent) {
             // TODO : implémenter
@@ -97,28 +111,30 @@ sap.ui.define([
          * @param {sap.ui.base.Event} oEvent
          * @public
          */
-        onButtonReadPressed: function(oEvent) {
-            var that = this;
-            var oHomeView = this.getView();
-            var oHomeViewModel = this.getModel("homeViewModel");
-            var sRequest = oHomeViewModel.getProperty("/request");
+        onButtonExecutePressed: function(oEvent) {
+            var iRequestType = this.getModel("homeViewModel").getProperty("/requestType"); 
+            
+            switch (iRequestType) {
+                case 0: // Read
+                    this._executeReadMethod();
+                    break;
 
-            oHomeView.setBusy(true);
-            this.getModel().read(sRequest, {
-                async: true,
-                success: function(oData, oResponse) {
-                    that._showMessageStrip(true);
-                    oHomeViewModel.setProperty("/response", JSON.stringify(oResponse));
+                case 1: // Create
+                    this._executeCreateMethod();
+                    break;
 
-                    oHomeView.setBusy(false);
-                },
-                error: function(oError) {
-                    that._showMessageStrip(false);
-                    oHomeViewModel.setProperty("/response", JSON.stringify(oError));
+                case 2: // Update
+                    this._executeUpdateMethod();
+                    break;
 
-                    oHomeView.setBusy(false);
-                }
-            });
+                case 3: // Remove
+                    this._executeRemoveMethod();
+                    break;
+
+                default:
+                    // TODO : not supported exception
+                    break;
+            }
         },
 
         /* =========================================================== */
@@ -126,15 +142,18 @@ sap.ui.define([
         /* =========================================================== */
 
         /**
+         * TODO : comment
          * @function
          * @param {sap.ui.base.Event} oEvent
-         * @public
+         * @private
          */
         _showMessageStrip: function(bIsSuccess) {
             var oMsgStrip = sap.ui.getCore().byId("msgStripId");
             var oHomePage = this.getView().byId("homePageId");
             var i18n = this.getModel("i18n").getResourceBundle();
             var oSettings = {};
+            var oPreviousControl = this.getView().byId("textAreaResponseQuery");
+            var iIndexPreviousControl = oHomePage.indexOfContent(oPreviousControl);
 
             // si il existe on le détruit
             if (oMsgStrip) {
@@ -158,8 +177,138 @@ sap.ui.define([
 
             oMsgStrip = new sap.m.MessageStrip("msgStripId", oSettings);
             // L'insert dans la page
-            // TODO : faire plutôt un getIndex
-            oHomePage.insertContent(oMsgStrip, 4);
+            oHomePage.insertContent(oMsgStrip, iIndexPreviousControl);
+        },
+        
+        /**
+         * Trigger a GET request to the odata service that was specified
+         * in the model constructor. The data will be stored in the model.
+         * The requested data is returned with the response.
+         * @function
+         * @private
+         */
+        _executeReadMethod: function() {
+            var oHomeView = this.getView();
+            var oHomeViewModel = this.getModel("homeViewModel");
+            var sQuery = oHomeViewModel.getProperty("/query");
+            var that = this;
+
+            oHomeView.setBusy(true);
+            this.getModel().read(sQuery, {
+                async: true,
+                success: function(oData, oResponse) {
+                    that._showMessageStrip(true);
+                    oHomeViewModel.setProperty("/response", JSON.stringify(oResponse));
+
+                    oHomeView.setBusy(false);
+                },
+                error: function(oError) {
+                    that._showMessageStrip(false);
+                    oHomeViewModel.setProperty("/response", JSON.stringify(oError));
+
+                    oHomeView.setBusy(false);
+                }
+            });
+        },
+
+        /**
+         * Trigger a POST request to the odata service that was specified
+         * in the model constructor. Please note that deep creates are not
+         * supported and may not work.
+         * @function
+         * @private
+         */
+        _executeCreateMethod: function() {
+            var oHomeView = this.getView();
+            var oHomeViewModel = this.getModel("homeViewModel");
+            var sQuery = oHomeViewModel.getProperty("/query");
+            var that = this;
+            var sJsonText = oHomeViewModel.getProperty("/jsonText");
+            // TDOO : add a try catch method to catch and validate JSON
+            var oData = JSON.parse(sJsonText);
+
+            oHomeView.setBusy(true);
+            this.getModel().create(sQuery, oData, {
+                async: true,
+                success: function(oData, oResponse) {
+                    that._showMessageStrip(true);
+                    oHomeViewModel.setProperty("/response", JSON.stringify(oResponse));
+
+                    oHomeView.setBusy(false);
+                },
+                error: function(oError) {
+                    that._showMessageStrip(false);
+                    oHomeViewModel.setProperty("/response", JSON.stringify(oError));
+
+                    oHomeView.setBusy(false);
+                }
+            });
+        },
+
+        /**
+         * Trigger a PUT/MERGE request to the odata service that was specified
+         * in the model constructor. The update method used is defined by the global
+         * defaultUpdateMethod parameter which is sap.ui.model.odata.UpdateMethod.Merge by default.
+         * Please note that deep updates are not supported and may not work.
+         * These should be done seperate on the entry directly. 
+         * @function
+         * @private
+         */
+        _executeUpdateMethod: function() {
+            var oHomeView = this.getView();
+            var oHomeViewModel = this.getModel("homeViewModel");
+            var sQuery = oHomeViewModel.getProperty("/query");
+            var that = this;
+            var sJsonText = oHomeViewModel.getProperty("/jsonText");
+            // TDOO : add a try catch method to catch and validate JSON
+            var oData = JSON.parse(sJsonText);
+
+            oHomeView.setBusy(true);
+            this.getModel().update(sQuery, oData, {
+                async: true,
+                success: function(oData, oResponse) {
+                    that._showMessageStrip(true);
+                    oHomeViewModel.setProperty("/response", JSON.stringify(oResponse));
+
+                    oHomeView.setBusy(false);
+                },
+                error: function(oError) {
+                    that._showMessageStrip(false);
+                    oHomeViewModel.setProperty("/response", JSON.stringify(oError));
+
+                    oHomeView.setBusy(false);
+                }
+            });
+        },
+
+        /**
+         * Trigger a DELETE request to the odata service that was specified
+         * in the model constructor. 
+         * @function
+         * @private
+         */
+        _executeRemoveMethod: function() {
+            var oHomeView = this.getView();
+            var oHomeViewModel = this.getModel("homeViewModel");
+            var sQuery = oHomeViewModel.getProperty("/query");
+            var that = this;
+
+            oHomeView.setBusy(true);
+            this.getModel().remove(sQuery, {
+                async: true,
+                success: function(oData, oResponse) {
+                    that._showMessageStrip(true);
+                    oHomeViewModel.setProperty("/response", JSON.stringify(oResponse));
+
+                    oHomeView.setBusy(false);
+                },
+                error: function(oError) {
+                    that._showMessageStrip(false);
+                    oHomeViewModel.setProperty("/response", JSON.stringify(oError));
+
+                    oHomeView.setBusy(false);
+                }
+            });
         }
 
     });
